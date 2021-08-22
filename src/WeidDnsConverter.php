3df8f2cb823afee113d447c77957281240985ef4
Till Wehowski <github@webfan.de>
2021-08-23 01:03:12 +0200

    Update WeidDnsConverter.php

--- src/WeidDnsConverter.php
+++ src/WeidDnsConverter.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 class WeidDnsConverter
 {
 	public static function host2dns(string $host):array {
@@ -10,16 +12,19 @@ class WeidDnsConverter
 	   return implode('.', array_reverse($dns));	
 	}
 	
-  	public static function weid2host(&$weid, $namespace='weid:', $base='9-DNS-7'):string {
-		if (stripos($weid, $namespace) !== 0)  return false; // wrong namespace
-      
-		$weid = explode(':', $weid, 2)[1]; // remove namespace
+  	public static function weid2host($weid, $namespace='weid:', $base='9-DNS-7'):string {
+        
+        $p=explode(':', $weid, 2);
+		if($namespace === $p[0] && count($p)===2){
+			 $weid=$p[1];
+		}
+		 
 
-		$elements = array_merge(explode('-', $base), explode('-', $weid));
+		$elements = explode('-', $weid);
 		$actual_checksum = array_pop($elements);
-		$expected_checksum = WeidHelper::weLuhnGetCheckDigit(implode('-',$elements));
+		$expected_checksum = \WeidHelper::weLuhnGetCheckDigit(implode('-',$elements));
 		if ($actual_checksum !== '?') {
-			if ($actual_checksum !== $expected_checksum) return false; // wrong checksum
+			if (!is_numeric($actual_checksum) || intval($actual_checksum) !== $expected_checksum) return false; // wrong checksum
 		} else {
 			// If checksum is '?', it will be replaced by the actual checksum,
 			// e.g. weid:EXAMPLE-? becomes weid:EXAMPLE-3
@@ -30,22 +35,25 @@ class WeidDnsConverter
 		//	$arc = strtoupper(self::base_convert_bigint($arc, 36, 10));
                    $arc = strtolower($arc);
 		}
+		
+		for($i=0;$i<count(explode('-', $base))-1;$i++){
+			array_shift($elements);
+		}
 		$host = implode('.', array_reverse($elements));
 
 		return $host;
 	}
 
-	public static function host2weid($host, $namespace='weid:', $base='9-DNS-7'):string {
+	public static function host2weid($host, $namespace='weid', $base='9-DNS-7'):string {
 		$elements = array_reverse(explode('.', $host));
 		foreach ($elements as &$arc) {
 		//			$arc = strtoupper(self::base_convert_bigint($arc, 10, 36));
                 	$arc = strtoupper($arc);
 		}
-		$weidstr = implode('-', $elements);
+		$weidstr =substr($base,0,strlen($base)-2).'-'. implode('-', $elements);		 
 
-		if (stripos($weidstr.'-', $base.'-') !== 0) return false; // wrong base
-
-		return $namespace . substr($weidstr.'-'.WeidHelper::weLuhnGetCheckDigit($weidstr), strlen($base.'-'));
+		return $namespace . ':'.$weidstr
+			    . '-'.\WeidHelper::weLuhnGetCheckDigit($weidstr);
 	}
   
 }
